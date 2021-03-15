3e02a7848e4b30f30799c8d8a1939c3ddd87d24d
George Dilthey <george.dilthey@gmail.com>
2021-03-14 20:06:05 -0400

    commit tree 86cf61d22cd729b9ebcf5fa7b108ecc28e712191

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,46 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    page = Nokogiri::HTML(open(index_url))
+    students = page.css('div.student-card')
+    students.map do |s|
+      name = s.css('div.card-text-container h4').text
+      location = s.css('div.card-text-container p').text
+      profile_url = s.css('a').attribute('href').value
+      {:name => name, :location => location, :profile_url => profile_url }
+
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    page = Nokogiri::HTML(open(profile_url))
+    student = {}
+    student[:profile_quote] = page.css('div.profile-quote').text
+    student[:bio] = page.css('div.bio-content div.description-holder p').text
+
+    socials = page.css('div.social-icon-container a')
+    socials.each do |s|
+      link = s.attribute('href').value
+      if link.match(/twitter/) 
+        student[:twitter] = link
+      elsif link.match(/linkedin/) 
+        student[:linkedin] = link
+      elsif link.match(/github/) 
+        student[:github] = link
+      else 
+        student[:blog] = link
+      end
+    end
+    student
+
   end
+
 
 end
 
+# scrape = Scraper.scrape_profile_page('students/ryan-johnson.html')
+# scrape
